30665a322725854da32c8e08b2f2802a99ec2c6c
flaviocp90 <flaviocoppola90@gmail.com>
2021-12-06 19:01:17 +0100

    Implement new part

--- client/src/screens/AddProduct.jsx
+++ client/src/screens/AddProduct.jsx
@@ -1,9 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
 
 const AddProduct = () => {
-    <div>
-        <h1>Add product</h1>
-    </div>
-}
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate(); 
 
-export default AddProduct
+  const addProductHandler = async (e) => {
+    
+    e.preventDefault()
+
+    const data = {
+      title: title,
+      price: price,
+      description: description,
+      published: true
+    }
+
+
+    await axios.post("/api/products/addProduct", data)
+    navigate('/products')
+
+  }
+
+  return (
+    <>
+      <Container className="mt-2 p-2">
+        <h1>Add product</h1>
+        <hr />
+        <Form onSubmit={addProductHandler}>
+          <Form.Group className="mb-5" controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="price">
+            <Form.Label>Price ($)</Form.Label>
+            <Form.Control
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+              type="number"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              as="textarea"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Add product
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default AddProduct;
--- client/src/screens/ProductDetail.jsx
+++ client/src/screens/ProductDetail.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Card, Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import { axios } from "axios";
+import { useParams } from 'react-router'
 
-const ProductDetail = () => {
+
+const ProductDetail = ({ product }) => {
+
+    const { id } = useParams()
+
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState("");
+  
+    
+    useEffect(() => {
+
+        const getSingleProductData = async () => {
+            const { data } = await axios.get(`/api/products/allProducts${id}`);
+            console.log(data);
+            setTitle(data.title)
+            setPrice(data.price)
+            setDescription(data.description)
+
+          };
+
+          getSingleProductData();
+
+          }, [])
+
     return (
         <div>
             <h1>Detail product</h1>
+            <Card
+        style={{ width: "18rem", margin: "5px" }}
+        className="shadow-lg m-2 p-3 "
+      >
+        <Card.Body>
+          <Card.Title>Title: {product.title}</Card.Title>
+          <Card.Title>Price: $ {product.price}</Card.Title>
+          <Card.Text>Description: {product.description}</Card.Text>
+        </Card.Body>
+        <Link to={`/product/edit/${product.id}`}>
+          <Button>Edit</Button>
+        </Link>
+        <Link to={`/product/${product.id}`}>
+          <Button>Delete</Button>
+        </Link>
+      </Card>
+
         </div>
     )
 }
