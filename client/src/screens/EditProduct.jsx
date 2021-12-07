e471c3237608eda4d357dbe6dc76175b2263ab4d
flaviocp90 <flaviocoppola90@gmail.com>
2021-12-07 18:58:03 +0100

    edit form

--- client/src/screens/EditProduct.jsx
+++ client/src/screens/EditProduct.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
 
 function EditProduct() {
-    return (
-        <div>
-            <h1>Edit product</h1>
-        </div>
-    )
+
+    const { id } = useParams();
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        
+        const getDataById = async () => {
+            const {data} = await axios.get(`/api/products/${id}`)
+            console.log(data)
+            setTitle(data.title)
+            setPrice(data.price)
+            setDescription(data.description)
+        }
+        getDataById();
+
+    },[id])
+
+    const updateHandler = () => {
+        console.log('update')
+    }
+  
+  return (
+    <div>
+      <Container className="mt-2 p-2">
+        <h1>Edit product</h1>
+        <hr />
+        <Form onSubmit={updateHandler}>
+          <Form.Group className="mb-5" controlId="title">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="price">
+            <Form.Label>Price ($)</Form.Label>
+            <Form.Control
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+              type="number"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              as="textarea"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Update product
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
 
-export default EditProduct
+export default EditProduct;
